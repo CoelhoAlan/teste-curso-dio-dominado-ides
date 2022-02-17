bdbffea9998f051babcfc96fcaed2d9cd488702a
CoelhoAlan <alan.coelho.d.santos@accenture.com>
2022-02-17 11:59:49 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,5 +11,41 @@ public class PrimeiroPrograma {
         /*    int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b) );*/
+
+        Livro livro1 = new Livro("dhsdhs", 5);
+    }
+
+    static class Livro{
+        private String nome;
+        private Integer numeroPagina;
+
+        public Livro(String nome, Integer numeroPagina) {
+            this.nome = nome;
+            this.numeroPagina = numeroPagina;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumeroPagina() {
+            return numeroPagina;
+        }
+
+        public void setNumeroPagina(Integer numeroPagina) {
+            this.numeroPagina = numeroPagina;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numeroPagina=" + numeroPagina +
+                    '}';
+        }
     }
 }
